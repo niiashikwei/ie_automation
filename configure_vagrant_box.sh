3da6da4728ecb1063a77014106a1d6ba968a4ad9
Nii <niiashikwei@hotmail.com>
2014-02-06 16:01:23 -0500

    Nii - separated out script to run server from configuration script

--- configure_vagrant_box.sh
+++ configure_vagrant_box.sh
@@ -25,7 +25,6 @@ echo "host ip is $host_ip"
 #get guest ip
 guest_ip=`VBoxManage guestproperty get "IE10 - Win7" '/VirtualBox/GuestInfo/Net/0/V4/IP' | awk '{print $NF}'`
 echo "guest ip is $guest_ip"
-export IE10_VM_IP="$guest_ip"
 
 #move to workdir
 cmd0="cd workdir"
@@ -35,22 +34,24 @@ selenium_version="selenium-server-standalone-2.39.0"
 cmd1="wget https://www.dropbox.com/s/yer2hc25lfvtjne/$selenium_version.jar -O $selenium_version.jar --no-check-certificate"
 
 #download ie driver, extract and add to path
-driver_path="C:\Users\IEUser\\"
 driver_version="IEDriverServer_Win32_2.39.0"
-cmd2="wget https://selenium.googlecode.com/files/$driver_version.zip -O $driver_version.zip --no-check-certificate"
-cmd3="unzip $driver_version.zip"
-cmd3="rm $driver_version.zip"
+cmd2="wget https://selenium.googlecode.com/files/$driver_version.zip -O drivers\\$driver_version.zip --no-check-certificate"
+cmd3="unzip drivers\\$driver_version.zip -d drivers"
+cmd4="rm drivers\\$driver_version.zip"
 
 #edit host file
-cmd4="echo $host_ip          localtest.me >>  C:\Windows\System32\drivers\etc\hosts"
-
-#run selenium server
-cmd5="start /min java -jar selenium-server-standalone-2.39.0.jar"
+cmd5="echo $host_ip          localtest.me >>  C:\Windows\System32\drivers\etc\hosts"
 
 cmds="$cmd0 && $cmd1 && $cmd2 && $cmd3 && $cmd4 && $cmd5"
 
 #ssh into vm and download server and driver
+echo downloading selenium server and driver
 sshpass -p password ssh ieuser@$guest_ip -p 2222 $cmds
 
-current_dir=`pwd`
-amplify && cd playlist-builder && ./cuke.sh && cd $current_dir
+#run selenium server
+#echo "starting selenium server"
+#nohup sshpass -p password ssh ieuser@$guest_ip -p 2222 "$cmd0 $cmd6"
+
+#echo "running cukes"
+#current_dir=`pwd`
+#amplify && cd playlist-builder && ./cuke.sh && cd $current_dir
---
+++ run_selenium_server.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+#get guest ip
+guest_ip=`VBoxManage guestproperty get "IE10 - Win7" '/VirtualBox/GuestInfo/Net/0/V4/IP' | awk '{print $NF}'`
+echo "guest ip is $guest_ip"
+export IE10_VM_IP="$guest_ip"
+
+#run selenium server
+cmd0="cd workdir"
+cmd1="java -jar selenium-server-standalone-2.39.0.jar"
+
+#run selenium server
+echo "starting selenium server"
+sshpass -p password ssh ieuser@$guest_ip -p 2222 "$cmd0 && $cmd1"
+
+#echo "running cukes"
+#current_dir=`pwd`
+#amplify && cd playlist-builder && ./cuke.sh && cd $current_dir
