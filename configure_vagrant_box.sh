4b517ab0285604ab3223a717812f1f8207b1d557
Nii <niiashikwei@hotmail.com>
2014-02-10 14:34:02 -0500

    Nii - some clean up and refactor

--- configure_vagrant_box.sh
+++ configure_vagrant_box.sh
@@ -17,31 +17,36 @@ echo "host ip is $host_ip"
 guest_ip=`VBoxManage guestproperty get "IE9 - Win7" '/VirtualBox/GuestInfo/Net/0/V4/IP' | awk '{print $NF}'`
 echo "guest ip is $guest_ip"
 
-#move to workdir
-cmd0="cd workdir"
+
+#create workdir
+sshpass -p Passw0rd! ssh ieuser@$guest_ip -p 2222 "mkdir workdir || echo \"directory already exists!\"" 
+
+function send_command {
+  sshpass -p Passw0rd! ssh ieuser@$guest_ip -p 2222 "cd workdir && $1"
+}
 
 #download selenium jar
 selenium_version="selenium-server-standalone-2.39.0"
-cmd1="wget https://www.dropbox.com/s/yer2hc25lfvtjne/$selenium_version.jar -O $selenium_version.jar --no-check-certificate"
+cmd2="wget -nc https://www.dropbox.com/s/yer2hc25lfvtjne/$selenium_version.jar -O $selenium_version.jar --no-check-certificate || echo 'selenium jar already present in directory'"
+send_command "$cmd2"
 
 #download ie driver, extract and add to path
+send_command "mkdir drivers || echo 'drivers directory already exists!'"
+
 driver_version="IEDriverServer_Win32_2.39.0"
-cmd2="wget https://selenium.googlecode.com/files/$driver_version.zip -O drivers\\$driver_version.zip --no-check-certificate"
-cmd3="unzip drivers\\$driver_version.zip -d drivers"
-cmd4="rm drivers\\$driver_version.zip"
+cmd3="cd drivers && wget -nc https://selenium.googlecode.com/files/$driver_version.zip --no-check-certificate || echo 'driver already downloaded!'"
+send_command "$cmd3"
+
+cmd4="cd drivers && unzip -u $driver_version.zip"
+send_command "$cmd4"
 
 #edit host file
-cmd5="echo $host_ip          localtest.me >>  C:\Windows\System32\drivers\etc\hosts"
+cmd6="echo $host_ip          localtest.me >>  C:\Windows\System32\drivers\etc\hosts"
+send_command "$cmd6"
 
 #seed commands
-cmd6="cd C:\Users\IEUser\\\"My Documents\""
-cmd7="touch default_file some_pdf.pdf some_ppt.ppt text_file.txt another_txt_file.txt"
-cmd8="cd C:\Users\IEUser\workdir"
-seed_cmds="$cmd6 && $cmd7 && $cmd8"
-
-cmds="$cmd0 && $cmd1 && $cmd2 && $cmd3 && $cmd4 && $cmd5 && $seed_cmds"
-
-#ssh into vm and download server and driver
-echo downloading selenium server and driver
-sshpass -p Passw0rd! ssh ieuser@$guest_ip -p 2222 $cmds
-
+cmd7="cd C:\Users\IEUser\\\"My Documents\""
+cmd8="touch default_file some_pdf.pdf some_ppt.ppt text_file.txt another_txt_file.txt"
+cmd9="cd C:\Users\IEUser\workdir"
+seed_cmds="$cmd7 && $cmd8 && $cmd9"
+send_command $seed_cmds
